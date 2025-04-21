67be567fb2b64e3c9f33308d0369173b198745c2
Denis Bakhvalov <dendibakh@gmail.com>
2025-04-21 18:50:01 -0400

    Add an option to exclude certain benchmarks

--- JetStreamDriverWithNode.js
+++ JetStreamDriverWithNode.js
@@ -33,13 +33,14 @@ JetStream.runCode = function (scripts) {
     return globalObject;
 }
 
-async function runAllBenchmarks() {
+async function runAllBenchmarks(excludeNames = []) {
+    JetStream.benchmarks = JetStream.benchmarks.filter(b => !excludeNames.includes(b.name));
     console.log("Total Benchmarks:", JetStream.benchmarks.length);
     await JetStream.initialize();
     await JetStream.start();
 }
 
-async function runSelectedBenchmarks(benchmarkNames) {
+async function runSelectedBenchmarks(benchmarkNames, excludeNames = []) {
     const benchmarkOptions = JetStream.benchmarks;
     const selectedBenchmarks = [];
 
@@ -53,15 +54,16 @@ async function runSelectedBenchmarks(benchmarkNames) {
         selectedBenchmarks.push(benchmark);
     }
 
-    JetStream.benchmarks = selectedBenchmarks;
+    // Filter out excluded benchmarks
+    JetStream.benchmarks = selectedBenchmarks.filter(b => !excludeNames.includes(b.name));
     await JetStream.initialize();
     await JetStream.start();
 }
 
 async function showUsage() {
     console.log("\nUsage:");
-    console.log("  npm run jetstream-node all [--output=filename.json]    # Run all benchmarks");
-    console.log("  npm run jetstream-node <name> [name2 ...] [--output=filename.json]  # Run specific benchmarks by name");
+    console.log("  npm run jetstream-node -- all [--output=filename.json] [--exclude=name1,name2]    # Run all benchmarks except excluded ones");
+    console.log("  npm run jetstream-node -- <name> [name2 ...] [--output=filename.json] [--exclude=name1,name2]  # Run specific benchmarks by name");
     console.log("\nAvailable benchmarks:");
     JetStream.benchmarks.forEach((benchmark, index) => {
         console.log(`  ${index + 1}. ${benchmark.name}`);
@@ -92,12 +94,19 @@ async function runJetStream() {
     try {
         const args = process.argv.slice(2);
         let outputFile = 'bench_result.json';
+        let excludeNames = [];
         
-        // Parse output file argument if present
+        // Parse output file and exclude arguments if present
         const outputArgIndex = args.findIndex(arg => arg.startsWith('--output='));
         if (outputArgIndex !== -1) {
             outputFile = args[outputArgIndex].split('=')[1];
             args.splice(outputArgIndex, 1);
+        }
+
+        const excludeArgIndex = args.findIndex(arg => arg.startsWith('--exclude='));
+        if (excludeArgIndex !== -1) {
+            excludeNames = args[excludeArgIndex].split('=')[1].split(',');
+            args.splice(excludeArgIndex, 1);
         }
         
         if (args.length === 0) {
@@ -108,11 +117,11 @@ async function runJetStream() {
         const command = args[0].toLowerCase();
 
         if (command === 'all') {
-            await runAllBenchmarks();
+            await runAllBenchmarks(excludeNames);
         } else {
             // Parse all arguments as benchmark names
             const benchmarkNames = args;
-            await runSelectedBenchmarks(benchmarkNames);
+            await runSelectedBenchmarks(benchmarkNames, excludeNames);
         }
 
         // Save results to JSON file
